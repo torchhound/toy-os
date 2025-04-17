93e42c1bed8300094f01ef3b06591b3054e1732c
torchhound <5600929+torchhound@users.noreply.github.com>
2025-04-16 19:37:41 -0500

    Move gdt so start is the first symbol

--- loader.asm
+++ loader.asm
@@ -1,16 +1,6 @@
 BITS 32
 global start
 
-gdt_start:
-    dq 0x0000000000000000
-    dq 0x00CF9A000000FFFF
-    dq 0x00CF92000000FFFF
-gdt_end:
-
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-
 start:
     mov esi, load_msg
     call print
@@ -58,3 +48,13 @@ print:
 
 load_msg:
     db 'Loading loader...', 0
+
+gdt_start:
+    dq 0x0000000000000000
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
